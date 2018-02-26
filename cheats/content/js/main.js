2994a1c8858d796650fb5e4939de0a8c8cf6759a
Zaydd <zayddhcf@gmail.com>
2018-02-26 17:10:45 +1000

    a new change.
    bye.

--- cheats/content/js/main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();});$(document).delegate('*[data-toggle="lightbox"]','click',function(event){event.preventDefault();$(this).ekkoLightbox();});
